cef552fa9955966551efbea097b5b0e33bfb25ab
Sifungurux <jonas_pedersen@hotmail.com>
2014-03-19 10:56:38 +0100

    Moves nagios passive to nagios-core Rep

--- Service reboot/service start.sh
+++ Service reboot/service start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 NOW=$(date +"%m-%d-%y %r")
 CURRENTDIR=`pwd`
-echo "test"
+
 /usr/sbin/service apache2 status
-STATUS=$?
-if [ ${STATUS} -eq 3 ]; then
+STATUS="$(/usr/sbin/service apache2 status )"
+
+if [ "$STATUS" = "Apache2 is NOT running." ]; then
         /usr/sbin/service apache2 start
         echo "At ${NOW} has Apache2 been rebootet " >> $CURRENTDIR/apacheScript.log 
 fi
